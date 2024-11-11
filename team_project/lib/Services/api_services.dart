662d08c9ec01628380b80c0dc0a492109fb24360
MathurakshiP <pmathu2126@gmail.com>
2024-11-11 22:32:38 +0530

    search bar add

--- team_project/lib/Services/api_services.dart
+++ team_project/lib/Services/api_services.dart
@@ -1,74 +1,32 @@
-
 import 'dart:convert';
-import 'dart:io';
 import 'package:http/http.dart' as http;
-import 'package:team_project/model/meal_plan_model.dart';
-import 'package:team_project/model/recipe_model.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class ApiServices {
-  ApiServices._instantiate();
-  static final ApiServices instance = ApiServices._instantiate();
+class ApiService {
+  final String _baseUrl = 'https://api.spoonacular.com/recipes/complexSearch';
 
-  final String baseURL ="https://api.spoonacular.com";
-  static const String API_KEY ="1f75b517cba74599aea9b389c2fd4013";
-  
-  Future<MealPlan> generateMealPlan({
-    required int targetCalories, 
-    required String diet
-    }) async{
+  Future<List<dynamic>> fetchRecipes(String query) async {
+    final response = await http.get(
+      Uri.parse('$_baseUrl?query=$query&apiKey=${dotenv.env['SPOONACULAR_API_KEY']}'),
+    );
 
-      if(diet=="None") diet ='';
-      Map<String, String> parameters ={
-        'timeFrame': 'day',
-        'targetCalories': targetCalories.toString(),
-        'diet':diet,
-        'apiKey':API_KEY,
-      };
-
-      Uri uri =Uri.https(
-        baseURL,
-        '/recipes/mealplans/generate',
-        parameters,
-      );
-
-      Map<String,String> headers={
-        HttpHeaders.contentTypeHeader:'application/json',
-      };
-
-      try{
-        var response = await http.get(uri, headers: headers);
-        Map<String,dynamic>data =json.decode(response.body);
-        MealPlan mealPlan= MealPlan.fromMap(data);
-        return mealPlan;
-      }catch(err){
-        throw err.toString();
-      }
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data['results'];
+    } else {
+      throw Exception('Failed to load recipes');
     }
+  }
 
-    Future<Recipe> fetchRecipe(String id) async{
-      Map<String,String> parameters = {
-        'includeNutrition':'false',
-        'apiKey':API_KEY,
-      };
+  Future<Map<String, dynamic>> fetchRecipeDetails(int id) async {
+    final response = await http.get(
+      Uri.parse('https://api.spoonacular.com/recipes/$id/information?apiKey=${dotenv.env['SPOONACULAR_API_KEY']}'),
+    );
 
-      Uri uri =Uri.https(
-        baseURL,
-        '/recipes/$id/information',
-        parameters,
-      );
-
-      Map<String,String> headers={
-        HttpHeaders.contentTypeHeader:'application/json',
-      };
-
-      try{
-        var response = await http.get(uri, headers: headers);
-        Map<String,dynamic>data =json.decode(response.body);
-        Recipe recipe= Recipe.fromMap(data);
-        return recipe;
-      }catch(err){
-        throw err.toString();
-      }
-
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load recipe details');
     }
+  }
 }
--- team_project/lib/main.dart
+++ team_project/lib/main.dart
@@ -1,21 +1,126 @@
-
 import 'package:flutter/material.dart';
-import 'package:team_project/Screens/search_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:team_project/Services/api_services.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  await dotenv.load();
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: RecipeScreen(),
+    );
+  }
+}
+
+class RecipeScreen extends StatefulWidget {
+  @override
+  _RecipeScreenState createState() => _RecipeScreenState();
+}
+
+class _RecipeScreenState extends State<RecipeScreen> {
+  final ApiService _apiService = ApiService();
+  TextEditingController _controller = TextEditingController();
+  List<dynamic> _recipes = [];
+  bool _isLoading = false;
+
+  void _searchRecipes() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      final recipes = await _apiService.fetchRecipes(_controller.text);
+      setState(() {
+        _recipes = recipes;
+      });
+    } catch (e) {
+      print(e);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  void _viewRecipeDetails(int id) async {
+    final details = await _apiService.fetchRecipeDetails(id);
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(details['title']),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(details['image']),
+              SizedBox(height: 10),
+              Text('Ready in: ${details['readyInMinutes']} minutes'),
+              SizedBox(height: 10),
+              Text(details['instructions'] ?? 'No instructions available.'),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Flutter Recipe App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.orange[500],
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recipe App'),
       ),
-      home: SearchScreen(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Search for a recipe',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: _searchRecipes,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            _isLoading
+                ? CircularProgressIndicator()
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: _recipes.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: EdgeInsets.symmetric(vertical: 8.0),
+                          child: ListTile(
+                            leading: Image.network(
+                              _recipes[index]['image'],
+                              width: 50,
+                              height: 50,
+                            ),
+                            title: Text(_recipes[index]['title']),
+                            subtitle: Text(
+                              'Ready in ${_recipes[index]['readyInMinutes']} minutes',
+                            ),
+                            onTap: () {
+                              _viewRecipeDetails(_recipes[index]['id']);
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+          ],
+        ),
+      ),
     );
-
   }
 }
