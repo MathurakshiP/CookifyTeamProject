74f0fc4d5e9b5d8fd133466322fc77234fbae140
ImaraMoh <imara.biz02@gmail.com>
2024-11-11 21:46:59 +0530

    add Login Page

--- team_project/lib/main.dart
+++ team_project/lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:team_project/Screens/login_page.dart';
 import 'package:team_project/Screens/search_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(
         primaryColor: Colors.orange[500],
       ),
-      home: const SearchScreen(),
+      home: const LoginPage(),
     );
 
   }
