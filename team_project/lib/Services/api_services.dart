2849659b09324eb3f8c26331615686387194430b
MathurakshiP <pmathu2126@gmail.com>
2024-11-11 22:36:04 +0530

    changes

--- team_project/lib/Services/api_services.dart
+++ team_project/lib/Services/api_services.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+
 class ApiService {
   final String _baseUrl = 'https://api.spoonacular.com/recipes/complexSearch';
 
